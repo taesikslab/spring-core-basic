03d2f97cffb803dcf30bf212517dcc1c8d64ed72
taesikyoo <yrns0907@gmail.com>
2021-01-11 10:47:18 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,9 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
+@MainDiscountPolicy
 @Component
 public class RateDiscountPolicy implements DiscountPolicy {
 

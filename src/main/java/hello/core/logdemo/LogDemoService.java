9698b728835655142952a1cb1cac26afef8639b2
taesikyoo <yrns0907@gmail.com>
2021-01-12 16:22:45 +0900

    빈 스코프

---
+++ src/main/java/hello/core/logdemo/LogDemoService.java
@@ -0,0 +1,17 @@
+package hello.core.logdemo;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LogDemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
